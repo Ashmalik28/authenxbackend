744f5890f804875e2ba00d695c2eb21ccae0a150
Ashyam Malik <137030701+Ashmalik28@users.noreply.github.com>
2025-11-16 16:33:43 +0530

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use("/uploads" , express.static(uploadDir));
 
 
 app.use(cors({
-  origin : "http://localhost:5173"
+  origin : "https://authenxfrontend1.vercel.app/"
 }))
 app.use(express.json());
 
