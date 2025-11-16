0594bc47e8b8f3bf355e82da7ce8b99ce17137b5
Ashyam Malik <137030701+Ashmalik28@users.noreply.github.com>
2025-11-16 15:51:37 +0530

    Remove MONGOURL logging from connectDB
    Removed console log of MONGOURL for security.

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    console.log(process.env.MONGOURL);
     try {
         await mongoose.connect(process.env.MONGOURL);
         console.log("MongoDB Connected ........")
