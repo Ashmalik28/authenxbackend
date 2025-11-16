93f02fd00aa3ede5f96cd6045190315d7c898a46
Ashyam Malik <137030701+Ashmalik28@users.noreply.github.com>
2025-11-16 20:04:17 +0530

    Set default port to 3000 if not specified

--- server.js
+++ server.js
@@ -577,6 +577,6 @@ app.get("/auth/check" , authMiddleware , (req , res) => {
 });
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000 , () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
